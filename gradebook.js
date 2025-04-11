2edfa27c659e85e71a168214542fd9cb286818e5
BenjaminPritchard07 <bpritchard@my.waketech.edu>
2025-04-11 18:53:57 -0400

    Gradebook
    Created Gradebook html and js files.

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+funtion fetchGradeData() {
+	console.log("Fetching grade data...");
+}
+
+function populateGradebook(data) {
+	console.log("Populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
